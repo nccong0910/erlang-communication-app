113071f6059ed3d12b05be0f75d588ab58dc3b5a
nccong0910 <nccong0910@gmail.com>
2022-03-25 10:53:56 +0700

    format code

--- src/chat_server.erl
+++ src/chat_server.erl
@@ -8,6 +8,7 @@
 -export([start_link/0, get_user/0, send_msg/4]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
+
 start_link() ->
     gen_server:start_link({global, ?SERVER}, ?MODULE, {}, []).
 
@@ -15,6 +16,7 @@ init({}) ->
 	State = [],
     {ok, State}.
 
+%% Public
 get_user() ->
 	gen_server:cast({global, ?SERVER}, {get_user, self()}).
 
@@ -22,6 +24,7 @@ send_msg(From, ToNode, ToClt, Msg) ->
 	io:format("Message from ~p to ~p~n", [From, ToClt]),
 	gen_server:call({ToClt, ToNode}, {message, From, Msg}).
 
+%% Private
 handle_call({connect, DestNode, DestClt}, _From, State) ->
 	io:format("____~p from ~p connecting...~n", [DestClt, DestNode]),
 	case lists:member(DestClt, State) of
